d8f19420eec7cbf29b33d61e5f28b5e872d3acdd
desilaupretre <desulisfr@gmail.com>
2021-04-19 22:43:03 +0200

    did some improvements on the pages with bootstraps and scss, a formulaire has a good form and added a quantity input on the page product.

--- js/produit.js
+++ js/produit.js
@@ -23,4 +23,17 @@ function productChoosen (teddy) {
         colorBear.appendChild(option); // cette option met dans le parent qui a une classe .card-color après select (ligne 10)
     }
 }
+
+function up(max) {
+    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+    if (document.getElementById("myNumber").value >= parseInt(max)) {
+        document.getElementById("myNumber").value = max;
+    }
+}
+function down(min) {
+    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+    if (document.getElementById("myNumber").value <= parseInt(min)) {
+        document.getElementById("myNumber").value = min;
+    }
+}
   
